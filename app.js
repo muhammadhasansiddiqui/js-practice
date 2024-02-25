12d1497afdc82720a3fa5e09d73438c6e479ecf9
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-02-25 18:20:35 +0500

    add

--- app.js
+++ app.js
@@ -1,54 +1,69 @@
 
-//---------------alert 1st chp paratice---------------
-
-
-alert("Error! please enter a vaild password.")
-
-
-alert("Welcome To Js Land \n Happy Coding")
-
-
-
-//---------------end alert 1st chp paratice---------------
-
-//--------------- alert 2 chp paratice---------------
-
-var fullName = ("Hasan")
-
-alert(fullName)
-
-var MyAge = ("18 years old")
-alert(MyAge)
-
-var Study = ("web dev")
-console.log(Study)
-
-var Eat = ("PIZZA \n PIZZ \n PIZ \n PI \n P ")
-console.log(Eat)
-
-var email =( "MY email address is example@example.com")
-
-console.log(email)
-
-alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
-
-//--------------end-alert 2 chp paratice---------------
-
-//--------------- alert 3 chp paratice---------------
-
-
-var myPHONE =("0333333333333")
-console.log(myPHONE)
-
-//---------------end- alert 3 chp paratice---------------
-
-
-//--------------- alert 4 chp paratice---------------
 
 
 
 
 
-//--------------- alert 4 chp paratice---------------
+
+
+// // //---------------alert 1st chp paratice---------------
+
+
+// // alert("Error! please enter a vaild password.")
+
+
+// // alert("Welcome To Js Land \n Happy Coding")
+
+
+
+// // //---------------end alert 1st chp paratice---------------
+
+// // //--------------- alert 2 chp paratice---------------
+
+// // var fullName = ("Hasan")
+
+// // alert(fullName)
+
+// // var MyAge = ("18 years old")
+// // alert(MyAge)
+
+// // var Study = ("web dev")
+// // console.log(Study)
+
+// // var Eat = ("PIZZA \n PIZZ \n PIZ \n PI \n P ")
+// // console.log(Eat)
+
+// // var email =( "MY email address is example@example.com")
+
+// // console.log(email)
+
+// // alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// // //--------------end-alert 2 chp paratice---------------
+
+// // //--------------- alert 3 chp paratice---------------
+
+
+// // var myPHONE =("0333333333333")
+// // console.log(myPHONE)
+
+// //---------------end- alert 3 chp paratice---------------
+
+
+// //--------------- alert 4 chp paratice---------------
+
+
+
+
+// var TOTALnum = +prompt("")
+
+
+// alert(TOTALnum / 2)
+
+// //--------------- alert 4 chp paratice---------------
+
+
+// if
+
 
 
