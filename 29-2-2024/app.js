040856e97c576e443c14dd42a49ffcc7316ec74f
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-02-29 20:10:40 +0500

    42 Variable for Booleans

--- 29-2-2024/app.js
+++ 29-2-2024/app.js
@@ -3,8 +3,18 @@
 var firstName = "hasan" ; 
 
 console.log(firstName);
-alert(firstName);
+// alert(firstName);
 
 var msg ="it's a car";
 
 console.log(msg);
+
+
+var a = 5;
+var b = 10;
+var c = a + b;
+
+console.log(c);
+
+var feepay = true ;
+console.log(feepay);
