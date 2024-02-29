5d49affec88944ae35418e301475dfb439df32c5
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-02-29 19:57:25 +0500

    work

--- 29-2-2024/app.js
+++ 29-2-2024/app.js
@@ -1,22 +1,5 @@
-var steps = document.getElementById("steps");
 
-function counter() {
-  setInterval(function () {
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var milliseconds = date.getMilliseconds();
 
-    // Format the time with leading zeros
-    hours = (hours % 12) || 12;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    milliseconds = milliseconds < 100 ? '0' + milliseconds : milliseconds;
+var firstName = "hasan" ; 
 
-    steps.innerHTML = `${hours}:${minutes}:${seconds}:${milliseconds}`;
-  }, 10);
-}
-
-// Call the counter function to start updating the time
-counter();
+console.log(firstName);
