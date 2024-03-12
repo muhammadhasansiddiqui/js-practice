61da632148801478d16b70535507337a6e968456
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-03-13 04:20:08 +0500

    work

--- 3-13-2024/app.js
+++ 3-13-2024/app.js
@@ -12,3 +12,11 @@
 
 // console.log(passingRatio++ , "passingRatio++")
 // console.log(passingRatio , "passingRatio")
+
+// var firstName = prompt ("enter your first name" , "hasan")
+
+// console.log(firstName.toUpperCase)
+
+// if (firstName.toUpperCase() == ++"HASAN") {
+//     prompt("welcome")
+// }
