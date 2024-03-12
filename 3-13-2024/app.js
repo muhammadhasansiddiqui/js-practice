1e45cebb8c873392fa8d83c80a755fbdca80db7b
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-03-13 04:10:01 +0500

    work

--- 3-13-2024/app.js
+++ 3-13-2024/app.js
@@ -2,3 +2,13 @@
 // var lastName = (77)
 
 // console.log( typeof firstName + " " + typeof lastName)
+
+
+// var  passingRatio = 70
+
+// console.log(++passingRatio)
+
+// console.log(--passingRatio)
+
+// console.log(passingRatio++)
+// console.log(passingRatio)
