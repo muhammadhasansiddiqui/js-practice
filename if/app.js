50416f2fc8812cee4c16dd5449c632721dd35c5b
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-03-14 23:57:23 +0500

    work

---
+++ if/app.js
@@ -0,0 +1,66 @@
+
+
+// if (false){
+
+//     alert("HELLO world")
+// }
+
+// var genter = prompt("Enter your gender" , "Male")
+// var  message = ("Not Match") 
+
+// if (genter === "Male"){
+//     message = ("Good Morning Sir")
+// }
+
+// if (genter === "Female"){
+//     message = ("Good Morning Ma'am")
+// }
+    
+// alert(message)
+        
+// var education = prompt("Enter your education")
+
+// if (education !== "Matric") {
+
+//     console.log("eligibal")
+// }
+
+// var age = prompt ("enter your age") ;
+// if (age >= 30) { 
+
+//     console.log("eligible");
+// }else {
+//     console.log("not eligible");
+// }
+
+// var age = prompt("Enter your age")
+// if (age <= 17) {
+//     alert("you are not eligible for CNIC")
+// }else {alert("eligible for CNIC")}
+
+// var firstName = prompt("Enter your first name")
+
+// if (firstName === "") {
+//     alert("Hello")
+// }
+// else{
+//     alert("WELLCOME")
+// }
+// var biryani = prompt("Enter your biryani shop")
+
+// if (biryani === "shop one") {
+//     alert("shop one ki biryani")
+// }   
+// else if (biryani === "shop two")
+// {
+//     alert("shop two ki biryani")
+// }
+// else if (biryani === "shop three")
+// {
+//     alert("shop three ki biryani")
+// }
+
+// else{
+
+//   alert("karachi wale ki biryani le ayo")
+// }
