3bab261d279039af46e9a88222170b61594f7bf1
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-03-17 02:56:17 +0500

    work

---
+++ Arrays/app.js
@@ -0,0 +1,11 @@
+
+// var student = [ "David Rayy", "Mike Thomas", "Jhonny Depp", "Jason Statham" ];
+
+// console.log(student);
+// console.log(student[2]);
+
+// console.log(student.length);
+
+// student [4] = "Bruce Wayne";
+// console.log(student);
+// console.log(student.length - 1 , "total" );
