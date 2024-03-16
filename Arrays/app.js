a21990dd9a8d3afbcba551d20071818f067376e8
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-03-17 03:03:00 +0500

    work

--- Arrays/app.js
+++ Arrays/app.js
@@ -9,3 +9,8 @@
 // student [4] = "Bruce Wayne";
 // console.log(student);
 // console.log(student.length - 1 , "total" );
+
+// var student = [ "David Rayy", "Mike Thomas", "abc" , "cd" , "hello"];
+
+// student [5] = "uzair"
+// console.log(student);
