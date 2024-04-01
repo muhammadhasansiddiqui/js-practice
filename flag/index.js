3085fa11e1cc361520714278e4d0385f0074f5f7
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-01 17:54:09 +0500

    work

--- flag/index.js
+++ flag/index.js
@@ -1,16 +1,49 @@
-var city = ["karachi", "lahore", "islamabad", "multan"];
-var flag = false;
+// var student = ["hasan" , "uzair"]
+// var flag = false;
+// for(var i = 0; i < student.length; i++){
+//     if(student[i] === "uzair"){
+//         flag = true;
+//         console.log("welcome uzair");
+    
+//     }
+// }
+// if(flag === false){
+//     console.log("not found");
+// }
 
-for (var i = 0; i < city.length; i++) {
-    if (city[i] === "multan") {
-        flag = true;
-        break;
-    }
 
-}
 
-if (flag) { 
-    console.log("Welcome to the city of lights");
-} else {
-    console.log("You are not welcome here");
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var city = ["karachi", "lahore", "islamabad", "multan"];
+// var flag = false;
+
+// for (var i = 0; i < city.length; i++) {
+//     if (city[i] === "multan") {
+//         flag = true;
+//         break;
+//     }
+
+// }
+
+// if (flag) { 
+//     console.log("Welcome to the city of lights");
+// } else {
+//     console.log("You are not welcome here");
+// }
