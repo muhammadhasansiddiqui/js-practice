0207e632b86b6dba15c6ee8c6663238714301863
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-06 02:25:11 +0500

    work

--- Changing case class 29/index.js
+++ Changing case class 29/index.js
@@ -31,3 +31,14 @@
 // }
 
 
+
+// var arr = [['ghous','ahmed'],
+
+//              ['zain','khan']]
+
+// for (var i = 0; i < arr.length; i++) 
+//     for (var j = 0; j < arr[i].length; j++) {
+//         console.log(arr[i][j])
+//     }
+
+
