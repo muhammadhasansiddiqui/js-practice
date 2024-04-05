3f8cc05758d3d71f7b3f0c49a0ae9d78c9bf8a11
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-05 16:59:04 +0500

    WORK

---
+++ Changing case class 29/index.js
@@ -0,0 +1,9 @@
+// var arr = [2,4,6,3,8,9,5,1 ,0.5]
+// var min = arr[0]
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         min = arr[i]
+//     }
+// }
+// console.log(min)
