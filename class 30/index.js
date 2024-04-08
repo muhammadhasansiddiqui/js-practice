ce25b37e45867d7738acba7aafb2791972848d70
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-09 03:22:08 +0500

    work

--- class 30/index.js
+++ class 30/index.js
@@ -10,15 +10,15 @@
 // for  (var i = 0; i < arr.length; i++) {
 
 //     for (var j = 0; j < arr[i].length; j++) {
-    
+
 //         console.log(arr[i][j]);
-    
+
 // }
 
 // }
 
 //Finding a character at a location
- 
+
 // var str = "Hasan Siddiqui";
 // console.log(str.charAt(1));
 
@@ -29,3 +29,6 @@
 
 // var str = "Hasan Siddiqui";
 // console.log(str.replace("Siddiqui","Khan"));
+
+// var str = "Pakistan and Saudi Arabia emphasise need for dialogue between Islamabad and New Delhi to resolve Kashmir dispute  Islamabad";
+// console.log(str.replace("Islamabad", "Karachi"));
