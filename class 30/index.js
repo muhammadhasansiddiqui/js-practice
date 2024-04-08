d50d4ed89caa59d67f9c743d11ecd29c5a6e9e1c
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-09 03:04:19 +0500

    work

---
+++ class 30/index.js
@@ -0,0 +1,11 @@
+// var arr = [["Hasan" , "Siddiqui"] , ["Uzair","Khan"]]
+
+// for  (var i = 0; i < arr.length; i++) {
+
+//     for (var j = 0; j < arr[i].length; j++) {
+    
+//         console.log(arr[i][j]);
+    
+// }
+
+// }
