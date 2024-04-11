185de1aa6a9b714b0d3c2a22fbaef08e75d67dc2
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-12 03:36:41 +0500

    work

--- class 31/index.js
+++ class 31/index.js
@@ -1,2 +1,16 @@
 // var firstName = "hasan siddiqui";
 // console.log(firstName.split( ""));
+
+//  var firstName = "hasan siddiqui-khan";
+//  console.log(firstName.split( "-"));
+
+// var firstName = ["hasan" ,"siddiqui"]
+// console.log(firstName.join( " "));
+
+
+// var firstName = ["hasan","siddiqui"]
+// console.log(firstName.reverse( " "));
+
+
+//  var firstName = ["hasan","siddiqui"]
+//  console.log(firstName.toString( ""));
