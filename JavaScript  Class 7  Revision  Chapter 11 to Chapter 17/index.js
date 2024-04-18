df2bbb5df2db5d7afafeefb9a9f7f2bd9ed82dbf
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-19 01:56:53 +0500

    work

--- JavaScript  Class 7  Revision  Chapter 11 to Chapter 17/index.js
+++ JavaScript  Class 7  Revision  Chapter 11 to Chapter 17/index.js
@@ -2,3 +2,14 @@
 //  ("karachi" == "karachi"){
 //   console.log("Welcome to the city of lights");
 //  }
+
+// var userName = prompt("Enter your name");
+
+// if (userName){
+//   console.log(userName);
+
+// }
+// else{
+
+//     alert("Please enter your name");
+// }
