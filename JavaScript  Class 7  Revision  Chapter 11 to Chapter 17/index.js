45d0d2461788a93b3afc2a63129b362784fe0d26
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-21 17:30:58 +0500

    work

--- JavaScript  Class 7  Revision  Chapter 11 to Chapter 17/index.js
+++ JavaScript  Class 7  Revision  Chapter 11 to Chapter 17/index.js
@@ -35,3 +35,28 @@
 // var arr = ["hasan", "uzair" , "zeeshan" , "rayyan", "mesum"];
 // arr.push("quid-e-azam" , "fashi")
 // console.log(arr);
+
+// var arr = ["hasan", "uzair" , "zeeshan" , "rayyan", "mesum"];
+//  arr.unshift("quid-e-azam" , "fashi")
+//  arr.shift()
+//  console.log(arr);
+
+
+
+
+// var students = ["hasan", "uzair" , "zeeshan" ];
+// students.splice(1,0, "rayyan", "mesum")
+// console.log(students);
+
+// var students = ["hasan", "uzair" , "zeeshan" , "ali"];
+// console.log(students.slice(0, 2));
+// console.log(students.slice(2));
+
+// var school = "smit"
+// console.log(school.slice(0,2));
+
+for (var i = 0; i <= 2; i++) {
+    console.log(i)
+    
+}
+console.log(i);
--- class 32/index.js
+++ class 32/index.js
@@ -1,8 +1,47 @@
-var work = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
-var  password = "";
 
-for (var i = 0; i < 10; i++) {
-    var work = Math.floor(Math.random() * work.length);
-    password += work;}
-    
-    console.log(password);
+
+// function addToCart() {
+//     var cartList = document.getElementById("cartList");
+//     var bur = document.getElementById("bur").value;
+//     var listItem = document.createElement("li");
+
+//     listItem.textContent = ;
+
+
+
+//     cartList.appendChild(listItem);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let cart = [];
+
+function addToCart(item) { const itemObj = { name: item, price: getItemPrice(item) }; cart.push(itemObj); updateCart(); }
+
+function getItemPrice(item) { switch (item) { case 'burger': return 10; case 'pizza': return 15; case 'pasta': return 12; default: return 0; } }
+
+function updateCart() { let total = 0; let cartList = ''; for (let i = 0; i < cart.length; i++) { cartList += <li>${cart[i].name} - $${cart[i].price}</li>; total += cart[i].price; } document.getElementById('cartList').innerHTML = cartList; document.getElementById('total').innerHTML = total; }
+
