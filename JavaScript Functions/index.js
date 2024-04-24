75dc299f7d110d321d6b54987298dc5db2145c8a
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-24 20:35:00 +0500

    work

---
+++ JavaScript Functions/index.js
@@ -0,0 +1,58 @@
+// function student(name , time) {
+    
+//     console.log("Hello Sir "+ " " + time);
+
+//     console.log("HI " +  "    My Name is" + name);
+    
+// }
+
+// let name = " Hasan"
+// let name1 = " Rahim"
+// let name2 = " Karim"
+// let name3 = " Jamal"
+
+// var time = " Good Morning"
+
+// student(name  , time )
+// student(name1 , time)
+// student(name2 , time)
+// student(name3 , time)
+
+
+// function student(name) {
+//     console.log("Hello " + name);
+// }
+
+// var studentName = prompt("Enter your name");
+// student(studentName)
+
+
+// function city(name, time = "Good Morning") {
+//     console.log("Hello!" +  time   + " wellcome to " + name + " Pakistan");
+    
+// }
+
+
+
+// var inputCity = prompt("Enter your city name");
+// city(inputCity);
+ 
+
+
+// function com(a, b) {
+//     if (a < b) {
+//         console.log("a is less than b");
+//     }
+    
+//     else if (a > b) {
+//         console.log("a is greater than b");
+//     }
+    
+//     else if (a === b) {
+//         console.log("a is equal to b");
+//     }
+// }
+
+// let a = 10 
+// let b = 20
+// com(a, b)
