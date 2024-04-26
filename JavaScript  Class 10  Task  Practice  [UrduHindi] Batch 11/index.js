9dcff19e31d74616c4c903d45c8313458d382953
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-27 02:22:59 +0500

    work

--- JavaScript  Class 10  Task  Practice  [UrduHindi] Batch 11/index.js
+++ JavaScript  Class 10  Task  Practice  [UrduHindi] Batch 11/index.js
@@ -1,4 +1,16 @@
-var str = null;
+// var str = null;
 
-str = " saylani";
-console.log(str);
+// str = " saylani";
+// console.log(str);
+// 13, 14, 15, 20
+// var arr = [11,13 ]
+
+
+var arr = [11, 13];
+
+for (var i = 0; i < arr.length ; i++) {
+
+  if (arr[i + 1] - arr[i] > 1) {
+    console.log(arr[i] + 1);
+  }
+}
