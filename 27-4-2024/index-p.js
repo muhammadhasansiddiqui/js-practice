a3512ea923339d3791452993f2341bca5d3face3
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-27 22:15:43 +0500

    work

---
+++ 27-4-2024/index-p.js
@@ -0,0 +1,15 @@
+function handlehover(tag) {
+
+    tag.innerText= "Hello Pakistan";
+    tag.style.color="green";
+
+    
+}
+
+function hover(tag) {
+
+    tag.innerText= "Hello";
+    tag.style.color="black";
+
+    
+}
