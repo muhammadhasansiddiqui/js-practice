0d4ed5e8a59272c488b58a9f3c1f69bd76e8078f
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-04 03:00:28 +0500

    work

--- Web & Mobile App Development Class 32  JAVASCRIPT  Weekdays (Sir Ghous)/index.js
+++ Web & Mobile App Development Class 32  JAVASCRIPT  Weekdays (Sir Ghous)/index.js
@@ -3,3 +3,12 @@
 // console.log(date.getDate);
 
 
+// var light = document.getElementById("bulbImg");
+
+// function bulbOn() {
+//     light.src = "https://t3.ftcdn.net/jpg/05/57/75/28/360_F_557752883_YyM4kmp9VLxEyjfhyRGgqGkb1OLQLwPU.jpg";
+// }
+    
+// function bulbOff() {
+//     light.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwSWoLx6UYL7YiXPT_8nW1N-NmCaZHCcxIpQ&s";
+// }
