859568d3496d9e9f77be77829c155d28caf8e1be
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-05 00:29:48 +0500

    work

---
+++ JavaScript  Class 14  Date Object  Functions  [UrduHindi] Batch 11/index.js
@@ -0,0 +1,33 @@
+
+
+
+// var weekDay = 
+// {
+//     0: "Sunday",
+//     1: "Monday",
+//     2: "Tuesday",
+//     3: "Wednesday",
+//     4: "Thursday",
+//     5: "Friday",
+//     6: "Saturday"
+// };
+// var day = new Date().getDay();
+
+// document.write(weekDay[day]);
+
+// var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// var date = new Date();
+// // var month = new Date().getDate();
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+
+// function date() {
+//     console.log(new Date());
+    
+// }
+// date();
+// date()
+// date()
+// date()
+// date()
