7cc1303421796a7d43734048934b667dfa4b067b
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-06 21:31:26 +0500

    work

---
+++ 6-5-2024/index.js
@@ -0,0 +1,71 @@
+
+// var allMarks = [
+//     {subject: 'math', marks: 80},
+//     {subject: 'english', marks: 75},
+//     {subject: 'hindi', marks: 65},
+//     {subject: 'science', marks: 85},
+//     {subject: 'social', marks: 75} 
+// ] 
+// var totalMarks = 0;
+
+// for(var i = 0; i < allMarks.length; i++){
+//     totalMarks += allMarks[i].marks;
+// }
+
+// console.log(totalMarks);
+
+
+// sir work
+
+// function submit() {
+//     var response = calc(5, 10);
+//     showSuccessMessage(response);
+// }
+
+// function showSuccessMessage(result) {
+//     alert("The sum is " + result);
+// }
+
+// const calc = (a, b) => a + b;
+
+// submit();
+
+// var studentResult = [-10, 39 ,49 ,38 ,-20]
+// var list =studentResult.filter(function(value){
+//     return value >= 0;
+// })
+
+// console.log(list);
+
+//normal fu
+
+// var studentResult = [-10, 39 ,49 ,38 ,-20];
+
+// function filterPositive(value) {
+//     return value >= 0;
+// }
+
+// var list = studentResult.filter(filterPositive);
+
+// console.log(list);
+
+// var calc = 0
+// list.forEach((num
+// ) =>{
+//     calc += num;
+// })
+// console.log(calc);
+
+// for (let i = 0; i < List.lenght; i++) {
+//     calc = calc + List[i];
+//     console.log(calc);
+// }
+
+// ==================================
+
+// Regex Function
+// /\S+@\S+\.\S+/
+
+//  var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//     var email = regex.test("abc@gmail.com");
+//     console.log(email);
