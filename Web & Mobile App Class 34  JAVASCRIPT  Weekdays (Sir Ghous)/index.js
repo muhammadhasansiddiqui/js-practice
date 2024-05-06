d4f593649af358ac261bf19e4104409f6dd35e8e
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-06 22:49:18 +0500

    work

---
+++ Web & Mobile App Class 34  JAVASCRIPT  Weekdays (Sir Ghous)/index.js
@@ -0,0 +1,23 @@
+// function cityOfPak() {
+//     var city = prompt("Enter your city name");
+//     if (city == "Karachi" || city == "karachi") {
+//         alert("Welcome to the city of lights");
+//     } else if (city === "") {
+//         alert("Enter your city name");
+//     } 
+//     else if (city == null) {
+//         alert("!! Enter your city name");
+//     }
+//     else {
+//         alert("Welcome to " + city + " city");
+//     }
+// }
+// cityOfPak();
+// function main() {
+
+//     alert("Welcome to the index.js");
+
+// }
+
+
+// main();
