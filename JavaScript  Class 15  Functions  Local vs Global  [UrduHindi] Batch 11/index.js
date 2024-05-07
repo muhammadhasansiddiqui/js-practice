42faf593a17c9c00da37689f3ea1925cdaaa4ed1
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-07 21:41:25 +0500

    work

--- JavaScript  Class 15  Functions  Local vs Global  [UrduHindi] Batch 11/index.js
+++ JavaScript  Class 15  Functions  Local vs Global  [UrduHindi] Batch 11/index.js
@@ -3,3 +3,48 @@
     
 // }
 // foo(5,15);
+
+// function abc(A) {
+    
+//     console.log(A);
+    
+// }
+
+// var arr  =["Hello World"]
+// abc(arr[0]);
+
+
+// function checkAttendance(student) {
+//     var lastClass = [101,102,103,104,];
+
+//     if (lastClass.includes(student)) {
+//         console.log("present");
+//     } else {
+//         console.log("absent");
+//     }
+    
+    
+// }
+// checkAttendance(101);
+// checkAttendance(102);
+// checkAttendance(103);
+// checkAttendance(104);
+// checkAttendance(105);
+
+// var count = 1;
+// function foo() {
+//     for (var i = 0; i < 3; i++) {
+//         count++;
+//         console.log(count);
+//         return count
+//     }
+// }
+
+// console.log(foo());
+
+// function abc() {
+//     console.log("test");
+//     return "test 12";
+//     console.log("test 3");
+// }
+// console.log(abc());
