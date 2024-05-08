c2302532c93e357c2295b1fbae8224f72847b21b
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-09 00:10:39 +0500

    work done

---
+++ Web & Mobile App Class 35  JAVASCRIPT  Weekdays (Sir Ghous)/index.js
@@ -0,0 +1,49 @@
+// function formSumbit() {
+//    var name = document.getElementById("name")
+//    var email= document.getElementById("email")
+//   if (name.value.trim() &&  email.value.trim()) {
+//     console.log("NAME === >" + name.value);
+//     console.log("EMAIL === >" + email.value);
+//     name.value= "";
+//     email.value= "";}
+//   else {
+//     alert("Please fill the form");}
+// }
+
+// var nameStudent = prompt("Enter your name");
+
+// switch (nameStudent) {
+//   case "Hasan":
+//     alert("Hello Hasan");
+//     break;
+
+//   case "Zee":
+//     alert("Hello Zee");
+//     break;
+
+//   case "Ahmad":
+//     alert("Hello Ahmad");
+//     break;
+
+//   default:
+//     if (nameStudent !== null) {
+//       alert("Hello Guest");
+//     }
+//     break;
+// }
+
+
+// var str = "5+5-";
+
+// var newInput = "10";
+// var last = str[str.length - 1]
+
+// var arr = ["+", "-", "*", "/"];
+// if (arr.indexOf(last) !== -1     &&  arr.indexOf(newInput) === -1 ) {
+//     str = str.slice(0, -1) + newInput;
+// }
+// else {
+//     str = str + newInput;
+// }
+
+// console.log("🚀 ~ str:", str);
