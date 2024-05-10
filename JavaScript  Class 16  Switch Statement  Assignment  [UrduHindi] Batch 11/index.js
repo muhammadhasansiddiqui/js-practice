0ae03527c0fcb056ad84966ec3fcf9890d256404
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-10 15:49:31 +0500

    WORK

---
+++ JavaScript  Class 16  Switch Statement  Assignment  [UrduHindi] Batch 11/index.js
@@ -0,0 +1,15 @@
+// var studentName = prompt("Enter your name");
+// studentName = studentName.toLowerCase();
+// switch (studentName) {
+//     case "ali":
+//         alert("Hello Sir Ali");
+//         break;
+//     case "sir ahmed": 
+//         alert("Hello Sir Ahmed");
+
+//         break;
+//         default:
+//         alert("Hello " + studentName);
+// }
+
+console.log(document)
