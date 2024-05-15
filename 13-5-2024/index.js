c065cd652f0c841be87f0ecc1f5a0eecdf535046
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-16 02:31:03 +0500

    work

--- 13-5-2024/index.js
+++ 13-5-2024/index.js
@@ -1,35 +1,43 @@
-// chat app
-var arr = 
-    [
-        {
-            "id": 1,
-            "name": "hasan",
-            "message": "Hello"
-        },
-        {
-            "id": 2,
-            "name": "uzair",
-            "message": "Hi"
-        },
-        {
-            "id": 3,
-            "name": "zee",
-            "message": "How are you?"
-        }
-    
-        ,
-        
-
-        
-
-    ];
-
- function sendMessage() {
-    var chat = document.getElementById("message-input");
-    chat.innerHTML = "";
-    for (var i = 0; i < arr.length; i++) {
-        chat.innerHTML += "<div class='chat-box'><p class='name'>" + arr[i].name + "</p><p class='message'>" + arr[i].message + "</p></div>";
+var messageList = [
+    { name: "user", msg: "Hi" },
+    { name: "women", msg: "jee sir, ap kasy hain" },
+    { name: "user", msg: "alhamdulillah" },
+    { name: "user", msg: "ap batain?" },
+    { name: "women", msg: "jee theek" },
+  ];
+  
+  const ul = document.getElementsByTagName("ul")[0];
+  
+  for (let i = 0; i < messageList.length; i++) {
+    ul.innerHTML += `<li id=${messageList[i].name === "user" ? "user" : ""}>
+          ${messageList[i].msg}</li>`;
+  }
+  
+  const message = document.getElementsByTagName("input")[0];
+  function handleSend() {
+    if (message.value !== "") {
+      ul.innerHTML += `<li id="user">${message.value}</li>`;
+      message.value = "";
+      setTimeout(() => autoReply(), 500);
+    }else{
+      alert("Please Type a Message!")
     }
-} {
-    
- }
+  }
+  function autoReply() {
+    var dummyMsgList = [
+      { name: "women", msg: "Hi" },
+      { name: "women", msg: "i don't know" },
+      { name: "women", msg: "alhamdulillah, mai theek hon" },
+      { name: "women", msg: "ap kahan rehty hain" },
+      { name: "women", msg: "ap reply kion ni karty" },
+    ];
+    var msgNumber = Math.floor(Math.random() * dummyMsgList.length);
+    ul.innerHTML += `<li>${dummyMsgList[msgNumber].msg}</li>`;
+  }  
+
+
+  messageInput.addEventListener("keydown", function(handleSend) {
+    if (event.key === "Enter") {
+        handleSend();
+    }
+});
