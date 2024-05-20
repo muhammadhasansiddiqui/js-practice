643b77f0f621654024ba8cfea988f0b88caa9978
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-20 15:51:34 +0500

    work

---
+++ 20-5-2024/index.js
@@ -0,0 +1,20 @@
+
+var newWord = document.createElement("h1");
+var newTxt = document.createTextNode(" new Hello!");
+
+newWord.appendChild(newTxt);
+document.body.appendChild(newWord);
+console.log(newWord);
+
+
+
+
+function add() {
+    
+    // var target = document.getElementById("img1");
+    target.setAttribute(
+      "src",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyTjeJUaD3MswsasRaok9NP4VI9a6sWzTksQ&s"
+    );
+}
+
---
+++ JavaScript  Class 17  Loops  Events  Assignment  [UrduHindi] Batch 11/index.js
@@ -0,0 +1,9 @@
+
+
+/* var arr = ["Hasan" ,'Zeeshan' , "Ali" , "Uzair" ] 
+
+for ( var v of arr){
+    console.log(v)} */
+
+
+    
