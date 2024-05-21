3978e18f5ad3b9244dc403ef2addaad2012727b6
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-21 19:06:04 +0500

    work

--- JavaScript  Class 17  Loops  Events  Assignment  [UrduHindi] Batch 11/index.js
+++ JavaScript  Class 17  Loops  Events  Assignment  [UrduHindi] Batch 11/index.js
@@ -1,9 +1,13 @@
-
-
 /* var arr = ["Hasan" ,'Zeeshan' , "Ali" , "Uzair" ] 
 
 for ( var v of arr){
     console.log(v)} */
 
-
-    
+function submit() {
+  var cities = document.getElementsByName("city");
+  for (var city of cities) {
+    if (city.checked) {
+      console.log(city.value);
+    }
+  }
+}
