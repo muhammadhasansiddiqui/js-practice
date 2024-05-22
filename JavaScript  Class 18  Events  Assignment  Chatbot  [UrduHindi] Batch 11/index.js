d7970f0320a1f9d8340ccef21eeea032d0865af7
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-22 14:13:30 +0500

    work

---
+++ JavaScript  Class 18  Events  Assignment  Chatbot  [UrduHindi] Batch 11/index.js
@@ -0,0 +1,38 @@
+// function swapImgage(event) {
+//   if (event.type === "mouseover") {
+//     event.target.src = "imag/pexels-photo-1421903.jpeg";
+//   } else {
+//     event.target.src =
+//       "imag/Photos-of-Abbotabad-Photo-of-PMA-Gymnasium-Kakul-Abbotabad-Pictures-of-Abbotabad.jpg";
+//   }
+// }
+
+
+// function fillCity() {
+//     var zip =document.getElementById("zip");
+//     var city = document.getElementById("city");
+
+//     switch (Number(zip.value) ){
+//         case 1:
+//         city.value="Karachi";
+//             break;
+//             case 2:
+//         city.value="Lahore";
+//             break;
+//             case 3:
+//             city.value="Islamabad";
+//                 break;
+    
+//         default:
+//             console.log("Invalid Zip");
+//     }
+    
+// }
+
+var input = document.getElementById("input");
+var list = document.getElementById("list");
+
+
+function (params) {
+    
+}
