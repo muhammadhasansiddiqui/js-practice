b68dfea030d306f90a99e372bb6af4e5c34e5f6c
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-23 15:33:36 +0500

    Moment.js

---
+++ JavaScript  Class 19  MomentJS  [UrduHindi] Batch 11/index.js
@@ -0,0 +1,6 @@
+// console.log (moment().format("MMMM  DD yyyy"));
+// var a = moment([2007, 0, 29]);
+// var a = moment([2007, 0, 28]);
+// var b = moment([2070, 0, 30]);
+// diff = b.diff(a, 'days');
+// console.log(diff);
