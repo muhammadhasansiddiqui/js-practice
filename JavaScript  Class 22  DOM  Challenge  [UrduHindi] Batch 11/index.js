5e29a1dc59115024151095861ec7495af28f0ef2
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-28 23:06:06 +0500

    work

--- JavaScript  Class 22  DOM  Challenge  [UrduHindi] Batch 11/index.js
+++ JavaScript  Class 22  DOM  Challenge  [UrduHindi] Batch 11/index.js
@@ -1,13 +1,67 @@
-var input = "{[()]}";
-var arr = [];
+// // var input = "{[()]}";
+// // var arr = [];
 
-for (var i = 0; i < input.length; i++) {
-    if (input[i] == "(" || input[i] == "{" || input[i] == "[") {
-        arr.push(input[i]);
-    }
+// // const e = require("express");
+
+// // for (var i = 0; i < input.length; i++) {
+// //     if (input[i] == "(" || input[i] == "{" || input[i] == "[") {
+// //         arr.push(input[i]);
+// //     }
     
 
-}
-console.log(arr);
+// // }
+// // console.log(arr);
     
     
+
+// function isBalanced(input) {
+//     var stack = [];
+
+//     for (var i = 0; i < input.length; i++) {
+// var x = input [i];
+
+// if  (x == "(" || x == "{" || x == "[") {
+//     stack.push(x);
+// }
+
+// else {
+//     if (stack.length == 0) {
+//         return  "NO";
+//     }
+//     var check;
+//     switch (x) {
+//         case ")":
+//             check = stack.pop();
+//             if (check == "{" || check == "[") {
+//                 return "NO";
+//             }
+//             break;
+//         case "}":
+//             check = stack.pop();
+//             if (check == "(" || check == "[") {
+//                 return "NO";
+//             }
+//             break;
+//         case "]":
+//             check = stack.pop();
+//             if (check == "(" || check == "{") {
+//                 return "NO";
+//             }
+//             break;
+//     }
+// if (stack.length) {
+//     return "NO"
+// }else {
+//     return "YES"
+// }}}}
+
+
+// console.log(isBalanced("{[()]}")); 
+// console.log(isBalanced("({[]})]"));
+// console.log(isBalanced("({[]})["));
+
+    
+
+
+    
+
