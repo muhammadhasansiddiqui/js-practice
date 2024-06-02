4bcb05942f20cc3c9bf6315940af984bc7294978
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-02 21:27:57 +0500

    work

--- JavaScript  Class 22  DOM  Challenge  [UrduHindi] Batch 11/index.js
+++ JavaScript  Class 22  DOM  Challenge  [UrduHindi] Batch 11/index.js
@@ -50,17 +50,75 @@
 //             break;
 //     }
 // if (stack.length) {
-//     return "NO"
+//     return "yes"
 // }else {
-//     return "YES"
+//     return "no"
 // }}}}
 
 
-// console.log(isBalanced("{[()]}")); 
-// console.log(isBalanced("({[]})]"));
+// console.log(isBalanced("{{}}[{}]()")); 
+// console.log(isBalanced("({[]}){{{}}}]"));
 // console.log(isBalanced("({[]})["));
 
+
+
+// function isBalanced(input) {
+//     var stack = [];
+
+//     for (var i = 0; i < input.length; i++) {
+//         var x = input[i];
+
+//         if (x == "(" || x == "{" || x == "[") {
+//             stack.push(x);
+//         } else {
+//             if (stack.length == 0) {
+//                 return "NO";
+//             }
+//             var check;
+//             switch (x) {
+//                 case ")":
+//                     check = stack.pop();
+//                     if (check != "(") {
+//                         return "NO";
+//                     }
+//                     break;
+//                 case "}":
+//                     check = stack.pop();
+//                     if (check != "{") {
+//                         return "NO";
+//                     }
+//                     break;
+//                 case "]":
+//                     check = stack.pop();
+//                     if (check != "[") {
+//                         return "NO";
+//                     }
+//                     break;
+//             }
+//         }
+//     }
+
+//     // Final check to see if the stack is empty
+//     if (stack.length == 0) {
+//         return "YES";
+//     } else {
+//         return "NO";
+//     }
+// }
+
+// console.log(isBalanced("[")); // YES
+// console.log(isBalanced("({[]}){{{}}}]")); // NO
+// console.log(isBalanced("({[)")); // NO
+
+
     
+// var para1 = document.getElementById("para1");
+// var e  = document.getElementsByClassName("h");
+// var f = document.getElementsByClassName("one");
+// console.log(para1);
+// console.log(e);
+// console.log(f);
+
 
 
     
