f4830bf0d738627ebe9edc69f20ac652782631bf
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-11 01:08:47 +0500

    class 2 done

--- JavaScript Class 02/app.js
+++ JavaScript Class 02/app.js
@@ -19,3 +19,37 @@
 
 // a = "Hasan"
 // console.log("Hello " + a)
+
+// var itCampus = "PIAIC"
+
+// console.log(Hello  )
+
+// assignment num # 5  
+// Q 1
+// var a = 3;
+// var b = 5;
+
+// document.write("sum of 3 and 5 is " + (a + b) + "<br>")
+
+// Q 2 
+
+
+// var a ;
+// console.log("🚀 ~ a:", a);
+// var a = 5
+// console.log("🚀 ~ a:", a);
+// var a = 5;
+// console.log("🚀 ~ a:", ++a);
+// var b = a + 7 ;
+// console.log("🚀 ~ b:", b);
+// var c = --b;
+// console.log("🚀 ~ c :", c );
+// var d = c % 3;
+// console.log("🚀 ~ d:", d);
+
+// Q 4
+
+// var ticketPrice = 600;
+// var totalCost = 600 * 5;
+// document.write("Total cost of buying 5 tickets is " + totalCost + "<br>")
+
