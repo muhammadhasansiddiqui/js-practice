f968de4ff37e6bcc852310d3b586463f36c17952
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-10 23:10:01 +0500

    claas 2

---
+++ JavaScript Class 02/app.js
@@ -0,0 +1,21 @@
+// var a = " Hasan"
+
+// // console.log ("Hello " + a)
+
+// document.write( ("Hello " +  a)  )
+
+// var a = " Hasan"
+// console.log("Hello " + a)
+
+// var  a = "zeeshan"
+// console.log("Hello " + a)
+
+//  var a = "omer"
+// console.log("Hello " + a)
+
+// var a 
+// console.log("🚀 ~ a :", a );
+
+
+// a = "Hasan"
+// console.log("Hello " + a)
