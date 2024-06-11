7836375081a9cafd783b13ef95897e2f94863a1e
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-11 20:07:06 +0500

    work

---
+++ JavaScript Class 03/app.js
@@ -0,0 +1,20 @@
+// Q NUM # 7
+
+// var priceOfIteam1 = 550;
+// var priceOfIteam2 = 500;
+// var oderiteam1q = 1;
+// var oderiteam2q = 2;
+// var shippingCharges = 50;
+
+// var totalCost =
+// priceOfIteam1 * oderiteam1q + priceOfIteam2 * oderiteam2q + shippingCharges;
+// console.log("🚀 ~  totalCost:",  totalCost);
+  
+
+// Q NUM # 8
+
+// var totalMarks = 980;
+// var marksObtained = 804;
+// var percentage = (marksObtained / totalMarks) * 100;
+// console.log("🚀 ~ your percentage is :", percentage );
+
