cf03e976132bda55fcf0e0e81c82079732b6cd90
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-11 22:48:49 +0500

    if else

---
+++ JavaScript Class 05/app.js
@@ -0,0 +1,16 @@
+// var companyHire = prompt(" enter your computer language ");
+// var age = prompt(" enter your age  ");
+// var gender = prompt(" enter your gender  ");
+
+// if (
+//   (companyHire === "html" && age >= 18 && gender === "male") ||
+//   companyHire === "css" ||
+//   companyHire === "javascript"
+// ) {
+//   var sarly = +prompt("enter your sarly");
+//   if (sarly >= 20000) {
+//     console.log("done");
+//   }
+// } else {
+//   console.log(" sorry you are not selected");
+// }
