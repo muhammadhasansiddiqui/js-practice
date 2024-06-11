2c09b0e53518e1c3b78f06a4dd3158c1ad3feaa9
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-11 21:09:18 +0500

    work

---
+++ JavaScript Class 04/app.js
@@ -0,0 +1,21 @@
+// var num1 = +prompt ("enter your value")
+// var opt = prompt ("enter your operator")
+// var num2 = +prompt ("enter your value")
+// var result; 
+
+// if (opt === "+" ){
+//     result = num1 + num2
+// }   
+// else if (opt === "-") {
+//     result = num1 - num2
+// }
+// else if (opt ===" *") {
+//     result = num1 * num2
+// }
+// else if (opt === "/") {
+//     result = num1 / num2
+// }
+// else {
+//     result = "invalid operator"
+// }
+// document.write(result)
