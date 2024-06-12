e7582c1a9df45f5da58b2b20e85cfe094c6c691d
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-13 02:02:23 +0500

    loop

---
+++ JavaScript Class 08/app.js
@@ -0,0 +1,11 @@
+// var num = +prompt("Enter a number")
+
+
+// for (var i = 1; i <= 10; i++ ) {if (num === num) {
+    
+//  document.write(  num+ ' x ' + i + "=" + num * i + "<br />")}
+//  else {
+//     document.write("Invalid Number ")
+//  }
+
+// }
