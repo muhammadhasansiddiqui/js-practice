d3d793723bc7418c6964b3fbea36db1270c101b8
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-12 23:33:24 +0500

    Assignment # 17-20 q=num1 to 3

---
+++ JavaScript Class 07/app.js
@@ -0,0 +1,59 @@
+// 1. Declare and initialize an empty multidimensional array.
+
+
+// var multiArray = [];
+// console.log("🚀 ~ multiArray:", multiArray);
+
+// 2. Declare and initialize a multidimensional array representing the following matrix:
+
+// var  matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+// console.log("🚀 ~ matrix:", matrix);
+
+
+
+// 3. Write a program to print numeric counting from 1 to 10.
+
+// var  counting = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log("🚀 ~ counting:", counting);
+
+// 4. Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user.
+
+
+
+
+
+
+
+
+
+
+
+
+// var nameOfstudent = [ "hasan" , "uzair" , " zeeshan"  ]
+
+
+// nameOfstudent.push("ali","zee" ,"abc")
+
+// console.log("🚀 ~ nameOfstudent:", nameOfstudent);
+
+// nameOfstudent.pop()
+// console.log("🚀 ~ nameOfstudent:", nameOfstudent);
+
+// var nameStudent = ["hasan", "uzair", "zeeshan"] 
+// nameStudent.shift() 
+// console.log("🚀 ~ nameStudent:", nameStudent);
+// nameStudent.unshift("ali", "zee", "abc")
+// console.log("🚀 ~ nameStudent:", nameStudent);
+
+
+// var cityName = ["karachi", "lahore", "peshawar", "quetta"]
+// cityName.splice(3, 4,)
+// cityName.splice(3, 0, "hyderabad")
+// console.log("🚀 ~ cityName:", cityName);
+
