fd5d8b5a5facc87b04147f0df761d1411a944a42
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-14 15:02:48 +0500

    work

---
+++ JavaScript Class 09/app.js
@@ -0,0 +1,12 @@
+// var userWord = prompt("Enter a word");
+
+// var splitstr = userWord.split("").reverse().join("");
+
+// if (userWord === splitstr ){
+//     console.log("This is a palindrome");
+
+// }else{
+//     console.log("This is not a palindrome");
+// }
+
+var ABC = "Hello world";
