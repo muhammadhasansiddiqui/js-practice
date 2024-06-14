429c216768ede3f51db22059b84f9ebc8b5ee05f
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-15 01:54:55 +0500

    chp 20 done

--- JavaScript Class 09/app.js
+++ JavaScript Class 09/app.js
@@ -10,3 +10,34 @@
 // }
 
 // var ABC = "Hello world";
+
+
+// var word = prompt("Enter a word");
+// var reverseWord = "";
+
+// for ( var  i = word.length - 1; i >= 0 ; i-- ) {
+//     reverseWord = reverseWord + word[i];
+// }  if (word === reverseWord) {
+//     console.log("This is a palindrome");
+// } else {
+//     console.log("This is not a palindrome");
+// }
+
+// for ( var i = 0; i < 5 ; i++  ) {
+//     for ( var j = 0; j < 5 ; j++ ) {
+//         console.log(i,j);
+//         break;
+//     }
+//     // break;
+// }
+
+//  var firstNames = ["BlueRay ", "Upchuck ", "Lojack"];
+//  var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+//  var fullNames = [];
+
+//  for (var i = 0; i < firstNames.length; i++) {
+//      for (var j = 0; j < lastNames.length; j++) {
+//          fullNames.unshift(firstNames[i] + " " + lastNames[j]);
+//      }
+//  }
+//  console.log(fullNames);
