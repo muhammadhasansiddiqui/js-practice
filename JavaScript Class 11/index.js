059901efc217ba1faef6c2a648ed2cd8005ef3cd
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-15 10:55:35 +0500

    work

---
+++ JavaScript Class 11/index.js
@@ -0,0 +1,10 @@
+// var  name = "hasan"
+// var fLetterC = name[0].toUpperCase()
+// var lLetterC = name.slice(1)
+// // console.log("🚀 ~ lLetterC:", lLetterC);
+// var fullName = fLetterC + lLetterC
+// console.log(fullName)
+
+// all word first leter capital 
+
+
