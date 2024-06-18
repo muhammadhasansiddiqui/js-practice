2e9b44f0d60f8daf2a6669fc54d18658863dd4e1
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-18 14:42:35 +0500

    work

---
+++ JavaScript  Class 24  Objects  [UrduHindi] Batch 11/app.js
@@ -0,0 +1,27 @@
+// // CA SHOWROOM OBJECTS
+// var cars = {
+//   // SEDAN
+//   Toyota: {
+//     Colrola: {
+//       name: "gli",
+//       price: 203000,
+//       image:
+//         "https://media.istockphoto.com/id/1359043512/photo/red-toyota-c-hr-car-moving-on-the-street.jpg?s=612x612&w=0&k=20&c=ssA46I3HReu8nUeqeEIxKxd8RjFy2R8gDvYc6FWg3Zk= ",
+//     },
+//     Honda: {
+//       name: "civic",
+//       price: 253000,
+//       image: "images/cars/sedan/sedan.jpg",
+//     },
+//     BMW: {
+//       name: "car bmw",
+//       price: 354000,
+//       image: "images/cars/sedan/convertible.jpg",
+//     },
+//   },
+// };
+// // delete cars.Toyota.Colrola.name;
+// // console.log("price" in cars.Toyota.Colrola);
+
+// cars.Toyota.Colrola.location = "karachi"
+// console.log(cars.Toyota.Colrola);
