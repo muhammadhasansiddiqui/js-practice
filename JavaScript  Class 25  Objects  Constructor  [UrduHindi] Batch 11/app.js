89b061af3690b1ee8e89b1585d3eb7f85aa9d55e
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-19 00:31:46 +0500

    work

---
+++ JavaScript  Class 25  Objects  Constructor  [UrduHindi] Batch 11/app.js
@@ -0,0 +1,58 @@
+// // // function Student (){
+
+// // // this.name = 'ahmed';
+// // // this.gender = 'male';
+// // // this.phone = 1234567890;
+// // // }
+// // // console.log(new Student())
+
+// // function Student (name , gender , phone ,city){
+
+// //     this.name = name;
+// //     this.gender = gender;
+// //     this.phone = phone;
+// //     this.city = city;
+// //     this.getDetail = function(){
+// //         return this.name ;
+// //     }
+// //  }
+
+// //  Student.prototype.getDetail= function (){
+
+// //  }
+// //     console.log(new Student("Ahemd", "Male", 1234567890,"Karachi" ))
+
+// //     var arr = [
+
+// // new Student("Ahemd", "Male", 1234567890, "Karachi" ),
+// // new Student("Ahemd", "Male", 1234567890, "Karachi" ),
+// // new Student("Hasan", "Male", 1234567890, "Karachi" ),
+// // new Student("Zeeshan", "Male", 1234567890, "Karachi" ),
+// // new Student("Uzair", "Male", 1234567890, "Karachi" ),
+// // new Student("Ahemd Raza ", "Male", 1234567890, "Karachi" ),
+// // new Student("Ahemd", "Male", 1234567890, "Karachi" ),
+// // new Student("Ahemd ALI", "Male", 1234567890, "Karachi" ),
+// // new Student("Ahemd", "Male", 1234567890, "Karachi" ),
+// // new Student("Ahemd", "Male", 1234567890, "Karachi" ),
+// // new Student("Ahemd", "Male", 1234567890, "Karachi" ),
+// // new Student("Ahemd", "Male", 1234567890, "Karachi" ),
+// // new Student("Ahemd", "Male", 1234567890, "Karachi" ),
+
+// //     ];
+// //     console.log("🚀 ~ arr:", arr);
+
+// var company = prompt("company");
+// var model = prompt("model");
+
+// var cars = {
+//   honda: {
+//     civic :{
+//         name: "civic",
+//         models: ["Accord", "Civic", "Insight"],
+//         color : "red"
+    
+//     },
+//   },
+//   };
+
+// console.log("🚀 ~ company:", cars[company][model]);
