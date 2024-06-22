57febaad852d6e6869b6e9e432cab758272e1d3d
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-22 16:07:23 +0500

    work

---
+++ JavaScript Class 22/app.js
@@ -0,0 +1,15 @@
+try{
+
+var name = "Hello World";
+console.log("🚀 ~ var:",name );
+// console.lo("🚀 ~ var:", name);
+
+
+throw "Something went wrong";
+
+
+} catch(err){
+    console.log(err);
+}
+
+console.log("karachi");
