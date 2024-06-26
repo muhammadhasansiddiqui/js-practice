c70ac97a067a738ba83d9d4314db7507bc07b068
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-27 01:45:39 +0500

    work

--- JavaScript Class 25/app.js
+++ JavaScript Class 25/app.js
@@ -1,7 +1,23 @@
 
 
-const arr = [1,2,3,4,5];
-const arr1=[6,7,8,9,10];
+// const arr = [1,2,3,4,5];
+// const arr1=[6,7,8,9,10];
 
-const arr2 = [...arr,...arr1];
-console.log("🚀 ~ arr2:", arr2);
+// const arr2 = [...arr,...arr1];
+// console.log("🚀 ~ arr2:", arr2);
+
+// var abc = "hello world";
+// // console.log("🚀 ~ abc:", abc);
+// let abc = "hello world";
+// // console.log("🚀 ~ abc:", abc);
+// var abc = "hello world";
+// const abc = "hello world";
+// console.log("🚀 ~ abc:", abc);
+
+// let obj = {name: "hasan", detail:{ school:"smit" } };
+// let obj2 ={...obj , detail:{...obj.detail  }};
+
+
+// obj2.detail.school = "siddiqui";
+// console.log("��� ~ obj:", obj);
+// console.log("��� ~ obj2:", obj2);
