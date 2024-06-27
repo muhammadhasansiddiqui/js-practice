b7ec8a2c5f567765b037fbd4d7e0da93b65a7fcb
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-27 23:05:22 +0500

    Exceptions: throw

---
+++ JavaScript  Class 28  Browser  Window  Validations  [UrduHindi] Batch 11/app.js
@@ -0,0 +1,57 @@
+
+
+
+// window.open("www.facebook.com")
+
+// console.log("hell0");
+
+// try
+// {
+// consol.log("hell0");
+// }
+// catch(err)
+// {
+//     console.log(err);
+// }
+
+// console.log("hell0");
+
+// throw example
+
+// console.log("hell0");
+// throw new Error("this is an error");
+// console.log("hell0");
+
+
+// try
+// {
+//     console.log("hell0");
+//     throw new Error("this is an error");
+//     console.log("hell0");
+// }
+// catch(err)
+// {
+//     console.log(err);
+// }
+
+// try
+// {
+//     setTimeout(function(){
+//         console.log("hell0");
+//     },3000);
+// }
+// catch(err)
+// {
+//     console.log(err);
+// 
+
+// var name = "";
+
+// try{
+
+// if (name === ""){
+//     throw ("name can not be empty");
+// }
+// }catch(err){
+//     console.log(err);
+// }
