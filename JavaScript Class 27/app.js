fd5014efc0698e26d926da41657943a31160ba35
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-30 22:19:05 +0500

    work

---
+++ JavaScript Class 27/app.js
@@ -0,0 +1,20 @@
+// class  Student {
+//     constructor(name,age,grade){
+//         this.name = name;
+//         this.age = age;
+//         this.grade = grade;
+//     }
+// }
+
+// let Student =new Student("John", 20, "A");
+
+
+// tarnari operator
+
+// let age =+prompt("enter your age");
+// let isAdult = age >= 18 ? "Are You Eligibale" : "Are You NoT Eligibale";
+// console.log("🚀 ~ isAdult:", isAdult);
+
+// let age =+prompt("enter your age");
+// let isAdult = age >= 18 ? "Are You Eligibale" : "Are You NoT Eligibale";
+// console.log("🚀 ~ isAdult:", isAdult);
