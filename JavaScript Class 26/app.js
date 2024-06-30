a53fbfe61ff0efc7942eaca81aef73971809b289
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-30 19:34:05 +0500

    done

--- JavaScript Class 26/app.js
+++ JavaScript Class 26/app.js
@@ -1,13 +1,32 @@
 // abc()
 // foo()
 // var foo =  function(){
-    
+
 //     console.log("hello");
 // }
-
 
 // function abc(){
 
 //     console.log("world");
 // }
 
+// arow funcation
+
+// var foo = (a) => {
+//   console.log(a);
+// };
+
+// foo("smit");
+
+// let foo = (a) => a * 5;
+
+// console.log(foo(2));
+
+// let obj = {
+
+//     name:"Hasan",
+//     getName:()=>{
+//         console.log(this);
+//     }
+// }
+// obj.getName(this);
