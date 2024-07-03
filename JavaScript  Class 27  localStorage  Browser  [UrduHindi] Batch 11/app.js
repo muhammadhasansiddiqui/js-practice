6be09e7ef4e41513c7c02b4e8298517fde6fd1a5
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-07-03 23:12:19 +0500

    Login/Signup

--- JavaScript  Class 27  localStorage  Browser  [UrduHindi] Batch 11/app.js
+++ JavaScript  Class 27  localStorage  Browser  [UrduHindi] Batch 11/app.js
@@ -1,56 +1,80 @@
-// function getuser() {
-//   var users = localStorage.getItem("users");
-//   if (users) {
-//     users = JSON.parse(users);
-//   } else {
-//     users = [];
-//   }
-//   return users;
-// }
+function getuser() {
+  var users = localStorage.getItem("users");
+  if (users) {
+    users = JSON.parse(users);
+  } else {
+    users = [];
+  }
+  return users;
+}
 
-// function register() {
-//   var name = document.getElementById("r-name");
-//   var email = document.getElementById("r-email");
-//   var password = document.getElementById("r-password");
-//   var users = getuser();
+function register() {
+  var name = document.getElementById("r-name");
+  var email = document.getElementById("r-email");
+  var password = document.getElementById("r-password");
+  var users = getuser();
 
-//   var user = {
-//     name: name.value,
-//     email: email.value,
-//     password: password.value,
-//   };
-//   users.push(user);
-//   localStorage.setItem("users", JSON.stringify(users));
+  var user = {
+    name: name.value,
+    email: email.value,
+    password: password.value,
+  };
+  if (name.value === "" || email.value === "" || password.value === "") {
+    Swal.fire("Please fill all input");
+    return;
+  }
+  users.push(user);
+  localStorage.setItem("users", JSON.stringify(users));
 
-//   name.value = "";
-//   email.value = "";
-//   password.value = "";
+  name.value = "";
+  email.value = "";
+  password.value = "";
+ 
 
-//   console.log("🚀 ~ register ~ users:", users);
-// }
+  Swal.fire("User registered successfully");
+  window.location.href = "index.html";
 
-// function login() {
-//   var email = document.getElementById("l-email");
-//   var password = document.getElementById("l-password");
-//   var users = getuser();
-//   var saveUser = {};
-//   invaild = false;
 
-//   for (let i = 0; i < users.length; i++) {
-//     if (
-//       email.value === users[i].email &&
-//       password.value === users[i].password
-//     ) {
-//       saveUser = users[i];
-//       invaild = true;
+  console.log("🚀 ", 
+    successfully = "User registered successfully" 
+  );
+}
 
-//       break;
-//     }
-//   }
+function login() {
+  var email = document.getElementById("l-email");
+  var password = document.getElementById("l-password");
+  var users = getuser();
+  var saveUser = {};
+  invaild = false;
 
-//   if (invaild) {
-//     console.log("user login success");
-//   } else {
-//     console.log("invalid email or password");
-//   }
-// }
+  if (email.value === "" || password.value === "") {
+    Swal.fire("Please fill all input");
+    return;
+  }
+  for (let i = 0; i < users.length; i++) {
+    if (
+      email.value === users[i].email &&
+      password.value === users[i].password
+    ) {
+      saveUser = users[i];
+      invaild = true;
+
+      break;
+    }
+
+  }
+
+  if (invaild) {
+    Swal.fire("User login successfully");
+    window.location.href = "welcome.html";
+
+    console.log("user login success");
+  } else {
+    Swal.fire("invalid email or password");
+
+    console.log("invalid email or password");
+  }
+}
+
+function logout(){
+  window.location.href = "index.html";}
