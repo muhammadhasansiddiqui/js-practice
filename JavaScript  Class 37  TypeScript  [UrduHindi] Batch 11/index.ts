65f07d7a3522ebb659d677180aa883a1b4f30b24
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-08 13:26:01 +0500

    TypeScript

---
+++ JavaScript  Class 37  TypeScript  [UrduHindi] Batch 11/index.ts
@@ -0,0 +1,8 @@
+// var num: number = 1;
+// var str: string = "string";
+// var bool: boolean = true;
+// var arr: number[] = [1, 2, 3];
+// var arr2: Array<number> = [1, 2, 3];
+// var any: any = 1;
+// var any2: any = "string";
+// var any3: any = true;
