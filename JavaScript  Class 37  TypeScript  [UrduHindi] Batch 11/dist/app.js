259dbacf0d636dfb03b78219d97ab1ce5c00346b
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-11 11:10:45 +0500

    TypeScript

--- JavaScript  Class 37  TypeScript  [UrduHindi] Batch 11/dist/app.js
+++ JavaScript  Class 37  TypeScript  [UrduHindi] Batch 11/dist/app.js
@@ -1,3 +1,8 @@
 "use strict";
 var fullName = "ABC";
 console.log("🚀 ~ fullName:", fullName);
+let arr = ["Hasan", true, 110];
+arr.push("Hassan");
+console.log("🚀 ~ arr:", arr);
+let arr2 = [110];
+arr2.push(10);
--- JavaScript  Class 37  TypeScript  [UrduHindi] Batch 11/src/app.ts
+++ JavaScript  Class 37  TypeScript  [UrduHindi] Batch 11/src/app.ts
@@ -1,2 +1,11 @@
 var fullName : string = "ABC";
 console.log("🚀 ~ fullName:", fullName);
+
+let arr: [string,boolean,number ] =["Hasan",true,110]
+
+arr.push("Hassan")
+
+console.log("🚀 ~ arr:", arr);
+
+let arr2: number[] = [110];
+arr2.push(10);
