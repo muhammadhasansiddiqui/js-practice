97a6e51706911f97ab85279977d4d15a7a9148fb
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-13 10:49:35 +0500

    TypeScript

--- JavaScript  Class 37  TypeScript  [UrduHindi] Batch 11/dist/app.js
+++ JavaScript  Class 37  TypeScript  [UrduHindi] Batch 11/dist/app.js
@@ -1,4 +1,47 @@
 "use strict";
 // var fullName : string = "ABC";
 // console.log("🚀 ~ fullName:", fullName);
-console.log("🚀 ~ ShirtSIZES:", 22 /* ShirtSIZES.Large */);
+// let arr: [string,boolean,number ] =["Hasan",true,110]
+// arr.push("Hassan")
+// console.log("🚀 ~ arr:", arr);
+// let arr2: number[] = [110];
+// arr2.push(10);
+// enum
+// const  enum ShirtSIZES {Samll = 20 ,Madium,Large}
+// console.log("🚀 ~ ShirtSIZES:", ShirtSIZES.Large);
+//  function abc(a:number, b:number):number{
+//     return a+b;
+// }
+// abc(10,20)
+// function abcd(a:number, b:number):void{
+//     console.log(a+b);
+// }
+// abc(10,20)
+// interface User {
+//     name: string;
+//     age: number;
+//     isStudent: boolean;
+//     email: string;
+//     classTime?: Array<string>;
+//     classDetails?: {
+//         subject: string;
+//         time: string;
+//     };
+//     getSTUDENTdata?: (a: number, b: number) => void;
+// }
+// let student: User = {
+//     name: "Hasan",
+//     age: 20,
+//     isStudent: true,
+//     email: "abc@gmail.com",
+// };
+// interface Class {
+//     classNAME: string;
+//     student: User;
+// }
+// let classDetails: Class = {
+//     classNAME: "WEB DEV",
+//     student: student,
+// };
+let abc = "smit";
+abc = 10;
