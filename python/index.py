12a53747a564c6ff778c7df9915edfc97c0b6b88
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-25 02:01:37 +0500

    python

--- python/index.py
+++ python/index.py
@@ -7,7 +7,7 @@ elif a == b:
   print("a and b are equal")
 
 
-#   age calutarer
+#   age 
 
 age = int(input("Enter your age: "))
 if age < 18:
@@ -30,4 +30,64 @@ if age < 18:
     print("You are an adult.")
     print("You are eligible to vote.")
 
+    print("You are eligible to drive.")
+    print("You are not eligible to marry.")
+    print("You are not eligible to drink.")
+    print("You are not eligible to vote.")
+
+        elif age >= 24 and age < 27:
+    print("You are an adult.")
+    print("You are eligible to vote.")
+
+    print("You are eligible to drive.")
+
+
+    # todo 
+
+
+    todo = input("Enter your todo: ")   
+
+    if todo == "study":
+        print("You should study for 2 hours.")
     
+    elif todo == "exercise":
+        print("You should exercise for 30 minutes.")
+        print("You should exercise for 30 minutes.")
+        print("You should exercise for 30 minutes.")
+        print("You should exercise for 30 minutes.")
+        print("You should exercise for 30 minutes.")
+        print("You should exercise for 30 minutes.")
+
+
+
+        # chat app fucationality
+
+        print("Welcome to the chat app!")
+        print("Please enter your name:")
+        name = input()
+    
+        print("Hello " + name + "! How are you today?")
+        feedback = input()
+        print("Thank you for your feedback!")
+        print("Have a great day!")
+        print("Have a great day!")
+        print("Have a great day!")
+    
+
+
+            elif todo == "sleep":
+        print("You should sleep for 8 hours.")
+
+        elif todo == "work":
+        print("You should work for 8 hours.")
+
+        elif todo == "eat":
+        print("You should eat for 2 hours.")
+        print("You should eat for 2 hours.")
+        print("You should eat for 2 hours.")
+
+
+            elif todo == "chat":
+        print("You should chat for 1 hour.")
+       
+       
