5deae9ed43e52a7e886b15ff8d4452cd8de1a34b
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-25 02:11:23 +0500

    py

--- python/index.py
+++ python/index.py
@@ -89,5 +89,92 @@ if age < 18:
 
             elif todo == "chat":
         print("You should chat for 1 hour.")
+
+
+
+        # arry to obj
+        arr = [1, 2, 3, 4, 5]
+        obj = {}
+        for i in arr:
+            obj[i] = i
+            print(obj)
+
+            # arry to obj
+            arr = [1, 2, 3, 4, 5]
+            obj = {}
+        
+        import socket
+import threading
+import tkinter as tk
+from tkinter import scrolledtext, simpledialog, messagebox
+
+# Server class to handle client connections and broadcasting messages
+class ChatServer:
+    def __init__(self, host='127.0.0.1', port=5000):
+        self.host = host
+        self.port = port
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.host, self.port))
+        self.server.listen()
+        self.clients = []
+        self.nicknames = []
+        print(f"Server started on {self.host}:{self.port}")
+
+    def broadcast(self, message, _client=None):
+        for client in self.clients:
+            if client != _client:
+                try:
+                    client.send(message)
+                except:
+                    client.close()
+                    self.remove_client(client)
+
+    def handle_client(self, client):
+        while True:
+            try:
+                message = client.recv(1024)
+                self.broadcast(message, client)
+            except:
+                self.remove_client(client)
+                break
+
+    def receive(self):
+        while True:
+            client, address = self.server.accept()
+            print(f"Connected with {str(address)}")
+
+            client.send('NICKNAME'.encode('utf-8'))
+            nickname = client.recv(1024).decode('utf-8')
+            self.nicknames.append(nickname)
+            self.clients.append(client)
+
+            print(f"Nickname is {nickname}")
+            self.broadcast(f'{nickname} joined the chat!'.encode('utf-8'))
+            client.send('Connected to the server!'.encode('utf-8'))
+
+            thread = threading.Thread(target=self.handle_client, args=(client,))
+            thread.start()
+
+    def remove_client(self, client):
+        if client in self.clients:
+            index = self.clients.index(client)
+            self.clients.remove(client)
+            client.close()
+            nickname = self.nicknames[index]
+            self.broadcast(f'{nickname} has left the chat!'.encode('utf-8'))
+            self.nicknames.remove(nickname)
+
+
+# Client class with GUI to send and receive messages
+class ChatClient:
+    def __init__(self, master, host='127.0.0.1', port=5000):
+        self.master = master
+        self.host = host
+        self.port = port
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client
+
+
+    
        
        
