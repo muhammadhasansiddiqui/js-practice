704c981b49d66b29fb5e2a45186e56823dfc6349
Hasan Siddiqui <143353727+muhammadhasansiddiqui@users.noreply.github.com>
2024-09-04 21:44:41 +0500

    Update firebase.js

--- JavaScript  Class 34  Firebase 🔥  [UrduHindi] Batch 11/firebase.js
+++ JavaScript  Class 34  Firebase 🔥  [UrduHindi] Batch 11/firebase.js
@@ -21,13 +21,9 @@ import { doc, setDoc,getFirestore } from "https://www.gstatic.com/firebasejs/9.6
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAFCfzc0RM7pWN54L79Q5mX4a2snu3VmA",
-  authDomain: "extended-legend-415308.firebaseapp.com",
-  projectId: "extended-legend-415308",
-  storageBucket: "extended-legend-415308.appspot.com",
-  messagingSenderId: "769368000323",
-  appId: "1:769368000323:web:186f4630f1d4118f6cd1f6",
-  measurementId: "G-FMMPHYKG3Y",
+
+// enter your keys
+  
 };
 
 // Initialize Firebase
